1e07ca4e77f38c38c0f5d84038054c53a80ba165
achillii <129614368+achillii@users.noreply.github.com>
2025-07-25 17:46:34 -0600

    Update achillighte-menu.js

--- achillighte-menu.js
+++ achillighte-menu.js
@@ -16,7 +16,7 @@ class AchillighteMenu extends HTMLElement {
         nav::before {
           content: "";
           position: absolute;
-          bottom: 5;
+          bottom: 0;
           left: 0;
           height: 1px;
           width: 100%;
